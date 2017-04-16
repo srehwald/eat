3c9a93638052a1fcd2e83cca9195e2e76be27c11
Simon Rehwald <simon.rehwald@tum.de>
2017-04-16 19:50:08 +0200

    add usage information

--- main.c
+++ main.c
@@ -5,6 +5,17 @@
 #include <time.h>
 #include <string.h>
 
+const char *locations[] = {"mensa-garching","mensa-arcisstrasse","stubistro-grosshadern"};
+const int locations_len = sizeof(locations) / sizeof(locations[0]);
+
+void print_usage() {
+    printf("Usage: eat <location> [<options>]\n");
+    // TODO do not hard code locations
+    printf("\t<location> \tavailable locations: mensa-garching, mensa-arcisstrasse, stubistro-grosshadern\n");
+    printf("\t-d DATE\t\tDate of the menu (default: current date); format: yyyy-mm-dd\n");
+}
+
+
 int main(int argc, char *argv[]) {
     int c;
 
@@ -14,8 +25,6 @@ int main(int argc, char *argv[]) {
     time_t t = time(NULL);
     struct tm date = *localtime(&t);
 
-    const char *locations[] = {"mensa-garching","mensa-arcisstrasse","stubistro-grosshadern"};
-    const int locations_len = sizeof(locations) / sizeof(locations[0]);
     char *location = NULL;
 
     /*
@@ -24,7 +33,7 @@ int main(int argc, char *argv[]) {
      * - https://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html
      */
     while (optind < argc) {
-        if ((c = getopt(argc, argv, "d:")) != -1) {
+        if ((c = getopt(argc, argv, "d:h")) != -1) {
             switch (c) {
                 case 'd':
                     // check if date conforms to required format
@@ -34,6 +43,9 @@ int main(int argc, char *argv[]) {
                     }
                     //printf("%d-%d-%d\n", date.tm_year+1900, date.tm_mon+1, date.tm_mday);
                     break;
+                case 'h':
+                    print_usage();
+                    return 0;
                 case '?':
                     if (optopt == 'd')
                         fprintf (stderr, "Option -%c requires an argument.\n", optopt);
@@ -43,6 +55,7 @@ int main(int argc, char *argv[]) {
                         fprintf (stderr,
                                  "Unknown option character `\\x%x'.\n",
                                  optopt);
+                    print_usage();
                     return 1;
                 default:
                     abort();
@@ -61,7 +74,7 @@ int main(int argc, char *argv[]) {
                 // if the passed location could'nt be found and therefore the variable isn't set return an error
                 if (location == NULL) {
                     fprintf(stderr, "Location '%s' not found.\n", argv[optind]);
-                    return 0;
+                    return 1;
                 }
             }
 
