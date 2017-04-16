e7425af0b294f95a3774092e2983f131ef6d3b0f
Simon Rehwald <simon.rehwald@tum.de>
2017-04-16 17:55:09 +0200

    basic cli skeleton

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    int c;
+
+    opterr = 0;
+
+    /*
+     * concepts partially adopted from:
+     * - http://stackoverflow.com/questions/18079340/using-getopt-in-c-with-non-option-arguments
+     * - https://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html
+     */
+    while (optind < argc) {
+        if ((c = getopt(argc, argv, "d:")) != -1) {
+            switch (c) {
+                case 'd':
+                    printf("date: %s\n", optarg);
+                    break;
+                case '?':
+                    if (optopt == 'd')
+                        fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                    else if (isprint (optopt))
+                        fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                    else
+                        fprintf (stderr,
+                                 "Unknown option character `\\x%x'.\n",
+                                 optopt);
+                    return 1;
+                default:
+                    abort();
+            }
+        } else {
+            // Regular argument
+            printf("arg: %s\n", argv[optind]);
+            optind++;
+        }
+    }
+
     return 0;
 }
