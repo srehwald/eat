17481db33f178ffc5177d9568ab7232e3743713e
Simon Rehwald <simon.rehwald@tum.de>
2017-04-20 21:56:52 +0200

    minimal working prototype

--- main.go
+++ main.go
@@ -5,11 +5,57 @@ import (
     "time"
     "fmt"
     "os"
+    "net/http"
+    "strconv"
+    "encoding/json"
+    "io/ioutil"
 )
+
+type Menu struct {
+    Number int16 `json:"number"`
+    Year int16 `json:"year"`
+    Days []Day `json:"days"`
+}
+
+type Day struct {
+    Date string `json:"date"`
+    Dishes []Dish `json:"dishes"`
+}
+
+type Dish struct {
+    Name string `json:"name"`
+    Price float64 `json:"price"`
+}
 
 // TODO better format notation
 const format = "2006-01-02"
+const api = "https://srehwald.github.io/stwm-mensa-api/"
 var currentDate = time.Now()
+
+
+func getMenu(location string, date time.Time) (*Menu, error) {
+    year := strconv.Itoa(date.Year())
+    _, w := date.ISOWeek()
+    week := strconv.Itoa(w)
+
+    // TODO leading zeros
+    url := api + location + "/" + year + "/" + week + ".json"
+    res, err := http.Get(url)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    var s = new(Menu)
+    unmarshalErr := json.Unmarshal(body, &s)
+
+    return s, unmarshalErr
+}
+
 
 func main() {
 
@@ -31,4 +77,12 @@ func main() {
     var location = args[0]
 
     fmt.Println("Menu for '"+ location+ "' on '" + date.Format(format) + "':")
+    menu, err := getMenu(location, date)
+
+    // TODO get correct date
+    day := menu.Days[0]
+
+    for _, dish := range day.Dishes {
+        fmt.Println(dish.Name + ": " + strconv.FormatFloat(dish.Price, 'f', -1, 64) + "€")
+    }
 }
